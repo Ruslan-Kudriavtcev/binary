144e4d80a575aae1b9e51556b8d2f7b9bee0a785
Руслан Кудрявцев <ruslan.kudriavtcev@gmail.com>
2020-05-17 22:52:17 +0300

    сверстали макет, сделал адаптив, паралакс главного экрана

--- js/script.js
+++ js/script.js
@@ -9,3 +9,23 @@ function mainblock() {
    minHeight: h
 }
 mainblock();
+
+$('.filter__item').click(function (event) {
+   let i = $(this).data('filter');
+   if (i == 1) {
+      $('.portfolio__column').show();
+   } else {
+      $('.portfolio__column').hide();
+      $('.portfolio__column.f-' + i).show();
+   }
+   $('.filter__item').removeClass('active');
+   $(this).addClass('active');
+
+   return false;
+});
+
+$(window).scroll(function (event) {
+
+   var s = 0 - $(this).scrollTop() / 2;
+   $('.mainblock__image').css('transform', 'translate3d(0, ' + s + 'px, 0)');
+});
